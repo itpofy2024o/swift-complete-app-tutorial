6665658d3b41edd4d7c8ee0c03cdfe71dd1cb56d
itpofy2024o <qweasdzxcf1G@proton.me>
2024-08-23 18:59:55 +0800

    annoyed

---
+++ swift-complete-app/swift_complete_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_complete_appApp.swift
+//  swift-complete-app
+//
+//  Created by Devor Vlad on 23/8/2024.
+//
+
+import SwiftUI
+
+@main
+struct swift_complete_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
