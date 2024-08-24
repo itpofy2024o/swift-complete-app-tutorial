fb2174bafdb4cf06b3a161eee10fea870074c212
itpofy2024o <qweasdzxcf1G@proton.me>
2024-08-24 22:39:01 +0800

    du xing sheng tan

--- swift-complete-app/Core/Auth/Views/AuthInputFieldView.swift
+++ swift-complete-app/Core/Auth/Views/AuthInputFieldView.swift
@@ -11,13 +11,25 @@ struct AuthInputFieldView: View {
     @Binding var text: String
     let label: String
     let placeholder: String
+    var isSecureInfo = false
     var body: some View {
         VStack(alignment: .leading) {
-            
+            Text(label)
+                .foregroundColor(Color(.darkText))
+                .fontWeight(.heavy)
+                .font(.footnote)
+            if isSecureInfo {
+                SecureField(placeholder,text:$text)
+                    .font(.system(size:15))
+            } else {
+                TextField(placeholder,text:$text)
+                    .font(.system(size: 15))
+            }
+            Divider()
         }
     }
 }
 
 #Preview {
-    AuthInputFieldView(text: .constant(""), label: "Gentle", placeholder: "Movie:")
+    AuthInputFieldView(text: .constant(""), label: "Gentle", placeholder: "Mov")
 }
