1877142c2720206b5bed1f8b7a2ad5daea4f4222
itpofy2024o <qweasdzxcf1G@proton.me>
2024-08-24 16:35:29 +0800

    fuck i hate this vertical spacing shit

--- swift-complete-app/Core/Auth/Views/AuthView.swift
+++ swift-complete-app/Core/Auth/Views/AuthView.swift
@@ -6,10 +6,21 @@
 //
 
 import SwiftUI
+import Kingfisher
 
 struct AuthView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .center) {
+            KFImage(URL(string:"https://moralis.io/wp-content/uploads/2023/11/Phantom-Wallet.png"))
+                .resizable()
+                .scaledToFit()
+                .frame(width:UIScreen.main.bounds.width*0.4)
+                .cornerRadius(25)
+            
+            VStack {
+                Text("Account").font(.title2).fontWeight(.heavy)
+            }.padding(.top,25)
+        }
     }
 }
 
