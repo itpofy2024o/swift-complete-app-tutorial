bc05aa8bb12730372ff2f9a6400ed4e907af7f67
itpofy2024o <qweasdzxcf1G@proton.me>
2024-08-28 15:18:12 +0800

    update

--- swift-complete-app/Core/ProfileInterface/Views/UserInfoCardView.swift
+++ swift-complete-app/Core/ProfileInterface/Views/UserInfoCardView.swift
@@ -27,16 +27,18 @@ struct UserInfoCardView: View {
             
             VStack(alignment: .leading) {
                 Text(username)
-                    .font(.title3)
+                    .font(UIDevice.current.userInterfaceIdiom == .pad
+                          ? .system(size: UIScreen.main.bounds.width*0.07) :.title3)
                     .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
                     .padding(.bottom,5)
                 Text(email)
-                    .font(.callout)
+                    .font(UIDevice.current.userInterfaceIdiom == .pad
+                          ? .system(size: UIScreen.main.bounds.width*0.048) :.callout)
                     .fontWeight(.regular)
                     .italic()
             }
         }
-        .padding(EdgeInsets(top: 15, leading: 20, bottom: 15, trailing: 20))
+        .padding(UIDevice.current.userInterfaceIdiom == .pad ? EdgeInsets(top: 35, leading: 50, bottom: 35, trailing: 50):EdgeInsets(top: 15, leading: 20, bottom: 15, trailing: 20))
         .background(colorScheme != .dark ? Color.gray.opacity(0.75) : Color.gray.opacity(0.25))
         .cornerRadius(20)
     }
