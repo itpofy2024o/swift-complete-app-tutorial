0895cd3cdba504d58550444a1e515b9febaf9804
itpofy2024o <qweasdzxcf1G@proton.me>
2024-08-28 20:12:51 +0800

    ahhhhh Firebase -> FirebaseAuth

---
+++ swift-complete-app/Core/Auth/ViewModels/AuthViewModel.swift
@@ -0,0 +1,44 @@
+//
+//  AuthViewModel.swift
+//  swift-complete-app
+//
+//  Created by Devor Vlad on 28/8/2024.
+//
+
+import Foundation
+import FirebaseAuth
+
+class AuthViewModel: ObservableObject {
+    @Published var userSession: FirebaseAuth.User?
+    @Published var currentUser: UserFirebase?
+    
+    init () {
+        
+    }
+    
+    func signIn(withEmail email:String, password: String) async throws {
+        
+    }
+    
+    func createNewUser(
+        withEmail email: String,
+        password: String,
+        username: String,
+        firstname: String,
+        lastname: String
+    ) async throws {
+        
+    }
+    
+    func singOut() {
+        
+    }
+    
+    func deleteYourAccount() {
+        
+    }
+    
+    func fetchUser() async {
+        
+    }
+}
